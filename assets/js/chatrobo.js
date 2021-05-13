7e5e1e4e1f09b3b651d7adb4e041e1edffeb13b7
mucahitsendinc <mucahit.sndc@gmail.com>
2021-05-14 02:35:49 +0300

    style tamamlandı js başlandı

--- assets/js/chatrobo.js
+++ assets/js/chatrobo.js
@@ -1,5 +1,33 @@
 $(function () {
-    $('#chatrobo').on('click', function () {
-       alert('asd');
+    var chatrobo = 'chatrobo';
+    var chatroboscreen = 'chatrobo-opened';
+    var chatroboClosebtn = 'chatroboClosebtn';
+    document.getElementById(chatroboscreen).style.display = "block";
+
+    $('#'+chatrobo).on('click', function () {
+        document.getElementById(chatrobo).style.display = "none";
+        document.getElementById(chatroboscreen).style.visibility = "visible";
+        document.getElementById(chatroboscreen).style.marginLeft ="0vw";
     });
+    $('#' + chatroboClosebtn).on('click', function () {
+        document.getElementById(chatrobo).style.display = "block";
+        document.getElementById(chatroboscreen).style.visibility = "hidden";
+        document.getElementById(chatroboscreen).style.marginLeft = "-100vw";
+    });
+    $('#chatroboForm').submit(function (e) {
+        e.preventDefault(); 
+
+        var form = $('#chatroboForm');
+        var url = 'app/chatroboPhp/test.php';
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: form.serialize(), // serializes the form's elements.
+            success: function (data) {
+                alert(data); // show response from the php script.
+            }
+        });
+    });
+    
 });
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>ChatRobo</title>
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="assets/css/chatrobo.css">
 	<link rel="stylesheet" href="assets/vendor/fontawesome/css/fontawesome-all.min.css">
 </head>
@@ -14,22 +14,23 @@
 		<div class="chatrobo">
 			<div class="chatrobo-title">
 				<h1>ChatRobo</h1>
-				<div class="chatrobo-closeBtn">X</div>
+				<div class="chatrobo-closeBtn" id="chatroboClosebtn">X</div>
 			</div>
-			<div class="chatrobo-messages">
+			<div class="chatrobo-messages" id="chatrobo-messages">
 				
-				<ul>
-					<li class="chatrobo-baloon send">
-						<div class="sender-title">
-							<a href="https://github.com/mucahitsendinc/chatrobo">ChatRobo</a>
-						</div>
-						<p>asdasd</p>
-					</li>
-					<li class="chatrobo-baloon sender">b</li>
-				</ul>
+				<div class="chatrobo-baloon send">
+					<div class="sender-title">
+						<a href="https://github.com/mucahitsendinc/chatrobo">ChatRobo</a>
+					</div>
+					<p>asdasdasdasdasdasdasdasdasdasdasdasdasdasd</p>
+				</div>
+				<div class="chatrobo-baloon sender">
+					<p>asdasd</p>
+				</div>
+					
 			</div>
 			<div class="chatrobo-messageform">
-				<form action="">
+				<form action="" id="chatroboForm" method="post">
 					<textarea name="message" id="message" rows="2" placeholder="Mesaj..."></textarea>
 					<button type="submit" value="1"><i class="fas fa-paper-plane"></i></button>
 				</form>
