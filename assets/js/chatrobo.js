3beb02bd280b9cca93318c149035b179abc7cc71
mucahitsendinc <mucahit.sndc@gmail.com>
2021-05-18 01:26:25 +0300

    Mesaj yüklenme delayı eklendi, mesaj gelme animasyonları eklendi

--- assets/js/chatrobo.js
+++ assets/js/chatrobo.js
@@ -7,9 +7,9 @@ $(function() {
     var messages = document.getElementById('chatrobo-messages');
     var sending = document.getElementById('message');
 
-    var messageHead = '<div class="chatrobo-baloon send"><div class="sender-title"><a target="_blank" href="https://github.com/mucahitsendinc/chatrobo" >ChatRobo</a></div><p>';
+    var messageHead = '<div class="chatrobo-baloon send animate__animated animate__slideInLeft"><div class="sender-title"><a target="_blank" href="https://github.com/mucahitsendinc/chatrobo" >ChatRobo</a></div><p>';
     var messageFoot = '</p></div>';
-    var smessageHead = '<div class="chatrobo-baloon sender"><p>';
+    var smessageHead = '<div class="chatrobo-baloon sender animate__animated animate__slideInRight""><p>';
     var smessageFoot = '</p></div>';
 
     document.getElementById(chatroboscreen).style.display = "block";
@@ -47,34 +47,37 @@ $(function() {
 
         var form = $('#chatroboForm');
         var url = 'app/chatroboPhp/index.php';
+        
         if (message.value.length > 0) {
+            messages.innerHTML = messages.innerHTML.replace("animate__animated animate__slideInLeft", "");
+            messages.innerHTML = messages.innerHTML.replace("animate__animated animate__slideInRight", "");
             messages.innerHTML = messages.innerHTML + smessageHead + message.value + smessageFoot;
             chatScroll();
 
-
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: form.serialize(), // serializes the form's elements.
-                success: function(data) {
-                    switch (data) {
-                        case "success":
-                            chatroboAdmin();
-                            break;
-                        case "clear":
-                            chatClear();
-                            break;
-                        case "quit":
-                            chatroboAdminOff();
-                            break;
-                        default:
-                            addMessage(data);
-                            break;
+            setTimeout(function () {
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: form.serialize(), // serializes the form's elements.
+                    success: function(data) {
+                        switch (data) {
+                            case "success":
+                                chatroboAdmin();
+                                break;
+                            case "clear":
+                                chatClear();
+                                break;
+                            case "quit":
+                                chatroboAdminOff();
+                                break;
+                            default:
+                                addMessage(data);
+                                break;
+                        }
                     }
-                }
-            });
-            message.value = "";
-
+                });
+                message.value = "";
+            }, 1000);
         }
     });
   
@@ -99,6 +102,8 @@ $(function() {
     }
 
     function addMessage(data) {
+        messages.innerHTML = messages.innerHTML.replace("animate__animated animate__slideInLeft", "");
+        messages.innerHTML = messages.innerHTML.replace("animate__animated animate__slideInRight", "");
         messages.innerHTML = messages.innerHTML + messageHead + data + messageFoot;
         chatScroll();
     }
